cf0fd4464a8ee142e9a510c0bfa460f8dcb45a5d
Christoph Rösmann <christoph.roesmann@tu-dortmund.de>
2015-05-20 12:56:58 -0700

    Fixed compilation issues on ubuntu saucy

--- cmake_modules/FindG2O.cmake
+++ cmake_modules/FindG2O.cmake
@@ -27,40 +27,40 @@ IF(UNIX)
   ENDIF (G2O_INCLUDE_DIR)
 
   FIND_LIBRARY(G2O_CORE_LIB             
-    NAMES g2o_core 
-    PATHS /usr/local /usr 
+    NAMES g2o_core g2o_core_rd
+    PATHS /usr/local /usr ${CMAKE_PREFIX_PATH}
     PATH_SUFFIXES lib)
   FIND_LIBRARY(G2O_STUFF_LIB            
-    NAMES g2o_stuff 
-    PATHS /usr/local /usr 
+    NAMES g2o_stuff g2o_stuff_rd
+    PATHS /usr/local /usr ${CMAKE_PREFIX_PATH}
     PATH_SUFFIXES lib)
   FIND_LIBRARY(G2O_TYPES_SLAM2D_LIB     
-    NAMES g2o_types_slam2d 
-    PATHS /usr/local /usr 
+    NAMES g2o_types_slam2d g2o_types_slam2d_rd
+    PATHS /usr/local /usr ${CMAKE_PREFIX_PATH}
     PATH_SUFFIXES lib)
   FIND_LIBRARY(G2O_TYPES_SLAM3D_LIB     
-    NAMES g2o_types_slam3d 
-    PATHS /usr/local /usr 
+    NAMES g2o_types_slam3d g2o_types_slam3d_rd
+    PATHS /usr/local /usr ${CMAKE_PREFIX_PATH}
     PATH_SUFFIXES lib)
   FIND_LIBRARY(G2O_SOLVER_CHOLMOD_LIB   
-    NAMES g2o_solver_cholmod 
-    PATHS /usr/local /usr 
+    NAMES g2o_solver_cholmod g2o_solver_cholmod_rd
+    PATHS /usr/local /usr ${CMAKE_PREFIX_PATH}
     PATH_SUFFIXES lib)
   FIND_LIBRARY(G2O_SOLVER_PCG_LIB       
-    NAMES g2o_solver_pcg 
-    PATHS /usr/local /usr 
+    NAMES g2o_solver_pcg g2o_solver_pcg_rd
+    PATHS /usr/local /usr ${CMAKE_PREFIX_PATH}
     PATH_SUFFIXES lib)
   FIND_LIBRARY(G2O_SOLVER_CSPARSE_LIB   
-    NAMES g2o_solver_csparse 
+    NAMES g2o_solver_csparse g2o_solver_csparse_rd
     PATHS /usr/local /usr 
     PATH_SUFFIXES lib)
   FIND_LIBRARY(G2O_INCREMENTAL_LIB      
-    NAMES g2o_incremental 
-    PATHS /usr/local /usr 
+    NAMES g2o_incremental g2o_incremental_rd
+    PATHS /usr/local /usr ${CMAKE_PREFIX_PATH}
     PATH_SUFFIXES lib)
   FIND_LIBRARY(G2O_CSPARSE_EXTENSION_LIB
-    NAMES g2o_csparse_extension
-    PATHS /usr/local /usr
+    NAMES g2o_csparse_extension g2o_csparse_extension_rd
+    PATHS /usr/local /usr ${CMAKE_PREFIX_PATH}
     PATH_SUFFIXES lib)
 
   SET(G2O_LIBRARIES ${G2O_CSPARSE_EXTENSION_LIB}
