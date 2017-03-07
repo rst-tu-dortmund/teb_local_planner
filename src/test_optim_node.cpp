fca41a54c82a4a50b965e58a495681d3878d24d6
Franz Albers <Franz.Albers@tu-dortmund.de>
2017-03-07 10:28:41 +0100

    Temporary commit for testing purposes

--- src/test_optim_node.cpp
+++ src/test_optim_node.cpp
@@ -137,9 +137,9 @@ int main( int argc, char** argv )
   RobotFootprintModelPtr robot_model = TebLocalPlannerROS::getRobotFootprintFromParamServer(n);
   
   // Setup planner (homotopy class planning or just the local teb planner)
-  if (config.hcp.enable_homotopy_class_planning)
-    planner = PlannerInterfacePtr(new HomotopyClassPlanner(config, &obst_vector, robot_model, visual, &via_points));
-  else
+//  if (config.hcp.enable_homotopy_class_planning)
+//    planner = PlannerInterfacePtr(new HomotopyClassPlanner(config, &obst_vector, robot_model, visual, &via_points));
+//  else
     planner = PlannerInterfacePtr(new TebOptimalPlanner(config, &obst_vector, robot_model, visual, &via_points));
   
 
