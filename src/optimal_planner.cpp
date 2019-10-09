520e14bf45affc8eb5a6b7e651908d7335463e1f
Victor Lopez <victor.lopez@pal-robotics.com>
2019-10-09 17:09:34 +0200

    Rename variable instead of scoping it's usage

--- src/optimal_planner.cpp
+++ src/optimal_planner.cpp
@@ -1249,17 +1249,17 @@ bool TebOptimalPlanner::isHorizonReductionAppropriate(const std::vector<geometry
     return false;
   
   // check if distance is at least 2m long // hardcoded for now
+ 
+  double traj_dist = 0;
+  for (int i=1; i < teb_.sizePoses(); ++i)
   {
-    double dist = 0;
-    for (int i=1; i < teb_.sizePoses(); ++i)
-    {
-      dist += ( teb_.Pose(i).position() - teb_.Pose(i-1).position() ).norm();
-      if (dist > 2)
-        break;
-    }
-    if (dist <= 2)
-      return false;
+    traj_dist += ( teb_.Pose(i).position() - teb_.Pose(i-1).position() ).norm();
+    if (traj_dist > 2)
+      break;
   }
+  if (traj_dist <= 2)
+    return false;
+  
   // check if goal orientation is differing with more than 90° and the horizon is still long enough to exclude parking maneuvers.
   // use case: Sometimes the robot accomplish the following navigation task:
   // 1. wall following 2. 180° curve 3. following along the other side of the wall.
