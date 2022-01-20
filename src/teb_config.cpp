16f1cc5ebd140f5f9ffd60ef1f7cacba9df8e09a
renan028 <renan028@gmail.com>
2022-01-20 02:25:11 -0300

    change to use std min

--- src/teb_config.cpp
+++ src/teb_config.cpp
@@ -355,7 +355,7 @@ void TebConfig::checkParameters() const
 
   // holonomic check
   if (robot.max_vel_y > 0) {
-    if (robot.max_vel_trans < robot.max_vel_x || robot.max_vel_trans < robot.max_vel_y) {
+    if (robot.max_vel_trans < std::min(robot.max_vel_x, robot.max_vel_trans)) {
       ROS_WARN("TebLocalPlannerROS() Param Warning: max_vel_trans < max_vel_x or max_vel_trans < max_vel_y. Note that vel_trans = sqrt(Vx^2 + Vy^2), thus max_vel_trans will limit Vx and Vy in the optimization step.");
     }
     
