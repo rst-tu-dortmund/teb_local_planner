fd47c2cee007df12b70f61084a0ab574b1bdd701
Guillaume Doisy <guillaume.doisy@wyca.fr>
2022-01-21 10:57:25 +0100

    remove map_frame param since it is set from costmap

--- teb_local_planner/src/teb_config.cpp
+++ teb_local_planner/src/teb_config.cpp
@@ -47,7 +47,6 @@ void TebConfig::declareParameters(const nav2_util::LifecycleNode::SharedPtr nh, 
   node_name = name;
 
   declare_parameter_if_not_declared(nh, name + "." + "odom_topic", rclcpp::ParameterValue(odom_topic));
-  declare_parameter_if_not_declared(nh, name + "." + "map_frame", rclcpp::ParameterValue(map_frame));
 
   // Trajectory
   declare_parameter_if_not_declared(nh, name + "." + "teb_autosize", rclcpp::ParameterValue(trajectory.teb_autosize));
@@ -175,7 +174,6 @@ void TebConfig::declareParameters(const nav2_util::LifecycleNode::SharedPtr nh, 
 void TebConfig::loadRosParamFromNodeHandle(const nav2_util::LifecycleNode::SharedPtr nh, const std::string name)
 {
   nh->get_parameter_or(name + "." + "odom_topic", odom_topic, odom_topic);
-  nh->get_parameter_or(name + "." + "map_frame", map_frame, map_frame);
   
   // Trajectory
   nh->get_parameter_or(name + "." + "teb_autosize", trajectory.teb_autosize, trajectory.teb_autosize);
