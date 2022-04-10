537adf4b1819df8588cbaf6479742b7b76517375
fengchuan.yin <1946253241@qq.com>
2022-04-10 15:37:34 +0800

    fix misc header

--- include/teb_local_planner/misc.h
+++ include/teb_local_planner/misc.h
@@ -40,112 +40,117 @@
 #define MISC_H
 
 #include <Eigen/Core>
-#include <boost/utility.hpp>
 #include <boost/type_traits.hpp>
+#include <boost/utility.hpp>
+#include <vector>
 
-
-namespace teb_local_planner
-{
+namespace teb_local_planner {
 
 #define SMALL_NUM 0.00000001
 
-//! Symbols for left/none/right rotations      
+//! Symbols for left/none/right rotations
 enum class RotType { left, none, right };
 
-/** 
+/**
  * @brief Check whether two variables (double) are close to each other
  * @param a the first value to compare
  * @param b the second value to compare
  * @param epsilon precision threshold
  * @return \c true if |a-b| < epsilon, false otherwise
  */
-inline bool is_close(double a, double b, double epsilon = 1e-4) 
-{ 
-  return std::fabs(a - b) < epsilon; 
+inline bool is_close(double a, double b, double epsilon = 1e-4) {
+  return std::fabs(a - b) < epsilon;
 }
 
-/** 
+/**
  * @brief Return the average angle of an arbitrary number of given angles [rad]
  * @param angles vector containing all angles
  * @return average / mean angle, that is normalized to [-pi, pi]
  */
-inline double average_angles(const std::vector<double>& angles)
-{
-  double x=0, y=0;
-  for (std::vector<double>::const_iterator it = angles.begin(); it!=angles.end(); ++it)
-  {
-      x += cos(*it);
-      y += sin(*it);
+inline double average_angles(const std::vector<double> &angles) {
+  double x = 0, y = 0;
+  for (std::vector<double>::const_iterator it = angles.begin();
+       it != angles.end(); ++it) {
+    x += cos(*it);
+    y += sin(*it);
   }
-  if(x == 0 && y == 0)
-      return 0;
+  if (x == 0 && y == 0)
+    return 0;
   else
-      return std::atan2(y, x);
+    return std::atan2(y, x);
 }
 
 /** @brief Small helper function: check if |a|<|b| */
-inline bool smaller_than_abs(double i, double j) {return std::fabs(i)<std::fabs(j);}
-
+inline bool smaller_than_abs(double i, double j) {
+  return std::fabs(i) < std::fabs(j);
+}
 
 /**
  * @brief Calculate a fast approximation of a sigmoid function
  * @details The following function is implemented: \f$ x / (1 + |x|) \f$
  * @param x the argument of the function
-*/
-inline double fast_sigmoid(double x)
-{
-  return x / (1 + fabs(x));
-}
+ */
+inline double fast_sigmoid(double x) { return x / (1 + fabs(x)); }
 
 /**
  * @brief Calculate Euclidean distance between two 2D point datatypes
  * @param point1 object containing fields x and y
  * @param point2 object containing fields x and y
  * @return Euclidean distance: ||point2-point1||
-*/
+ */
 template <typename P1, typename P2>
-inline double distance_points2d(const P1& point1, const P2& point2)
-{
-  return std::sqrt( std::pow(point2.x-point1.x,2) + std::pow(point2.y-point1.y,2) );
+inline double distance_points2d(const P1 &point1, const P2 &point2) {
+  return std::sqrt(std::pow(point2.x - point1.x, 2) +
+                   std::pow(point2.y - point1.y, 2));
 }
 
-
 /**
- * @brief Calculate the 2d cross product (returns length of the resulting vector along the z-axis in 3d)
+ * @brief Calculate the 2d cross product (returns length of the resulting vector
+ * along the z-axis in 3d)
  * @param v1 object containing public methods x() and y()
  * @param v2 object containing fields x() and y()
  * @return magnitude that would result in the 3D case (along the z-axis)
-*/
+ */
 template <typename V1, typename V2>
-inline double cross2d(const V1& v1, const V2& v2)
-{
-     return v1.x()*v2.y() - v2.x()*v1.y();
+inline double cross2d(const V1 &v1, const V2 &v2) {
+  return v1.x() * v2.y() - v2.x() * v1.y();
 }
 
-/** 
- * @brief Helper function that returns the const reference to a value defined by either its raw pointer type or const reference.
- * 
+/**
+ * @brief Helper function that returns the const reference to a value defined by
+ * either its raw pointer type or const reference.
+ *
  * Return a constant reference for boths input variants (pointer or reference).
- * @remarks Makes only sense in combination with the overload getConstReference(const T& val).
+ * @remarks Makes only sense in combination with the overload
+ * getConstReference(const T& val).
  * @param ptr pointer of type T
- * @tparam T arbitrary type 
- * @return  If \c T is a pointer, return const *T (leading to const T&), otherwise const T& with out pointer-to-ref conversion
+ * @tparam T arbitrary type
+ * @return  If \c T is a pointer, return const *T (leading to const T&),
+ * otherwise const T& with out pointer-to-ref conversion
  */
-template<typename T>
-inline const T& get_const_reference(const T* ptr) {return *ptr;}
+template <typename T> inline const T &get_const_reference(const T *ptr) {
+  return *ptr;
+}
 
-/** 
- * @brief Helper function that returns the const reference to a value defined by either its raw pointer type or const reference.
- * 
+/**
+ * @brief Helper function that returns the const reference to a value defined by
+ * either its raw pointer type or const reference.
+ *
  * Return a constant reference for boths input variants (pointer or reference).
- * @remarks Makes only sense in combination with the overload getConstReference(const T* val).
+ * @remarks Makes only sense in combination with the overload
+ * getConstReference(const T* val).
  * @param val
  * @param dummy SFINAE helper variable
- * @tparam T arbitrary type 
- * @return  If \c T is a pointer, return const *T (leading to const T&), otherwise const T& with out pointer-to-ref conversion
+ * @tparam T arbitrary type
+ * @return  If \c T is a pointer, return const *T (leading to const T&),
+ * otherwise const T& with out pointer-to-ref conversion
  */
-template<typename T>
-inline const T& get_const_reference(const T& val, typename boost::disable_if<boost::is_pointer<T> >::type* dummy = 0) {return val;}
+template <typename T>
+inline const T &get_const_reference(
+    const T &val,
+    typename boost::disable_if<boost::is_pointer<T>>::type *dummy = 0) {
+  return val;
+}
 
 } // namespace teb_local_planner
 
--- src/test_optim_node.cpp
+++ src/test_optim_node.cpp
@@ -41,11 +41,11 @@
 #include <interactive_markers/interactive_marker_server.h>
 #include <visualization_msgs/Marker.h>
 
-#include <boost/shared_ptr.hpp>
 #include <boost/make_shared.hpp>
+#include <boost/shared_ptr.hpp>
 
-
-using namespace teb_local_planner; // it is ok here to import everything for testing purposes
+using namespace teb_local_planner; // it is ok here to import everything for
+                                   // testing purposes
 
 // ============= Global Variables ================
 // Ok global variables are bad, but here we only have a simple testing node.
@@ -54,7 +54,8 @@ TebVisualizationPtr visual;
 std::vector<ObstaclePtr> obst_vector;
 ViaPointContainer via_points;
 TebConfig config;
-boost::shared_ptr< dynamic_reconfigure::Server<TebLocalPlannerReconfigureConfig> > dynamic_recfg;
+boost::shared_ptr<dynamic_reconfigure::Server<TebLocalPlannerReconfigureConfig>>
+    dynamic_recfg;
 ros::Subscriber custom_obst_sub;
 ros::Subscriber via_points_sub;
 ros::Subscriber clicked_points_sub;
@@ -62,56 +63,66 @@ std::vector<ros::Subscriber> obst_vel_subs;
 unsigned int no_fixed_obstacles;
 
 // =========== Function declarations =============
-void CB_mainCycle(const ros::TimerEvent& e);
-void CB_publishCycle(const ros::TimerEvent& e);
-void CB_reconfigure(TebLocalPlannerReconfigureConfig& reconfig, uint32_t level);
-void CB_customObstacle(const costmap_converter::ObstacleArrayMsg::ConstPtr& obst_msg);
-void CreateInteractiveMarker(const double& init_x, const double& init_y, unsigned int id, std::string frame, interactive_markers::InteractiveMarkerServer* marker_server, interactive_markers::InteractiveMarkerServer::FeedbackCallback feedback_cb);
-void CB_obstacle_marker(const visualization_msgs::InteractiveMarkerFeedbackConstPtr &feedback);
-void CB_clicked_points(const geometry_msgs::PointStampedConstPtr& point_msg);
-void CB_via_points(const nav_msgs::Path::ConstPtr& via_points_msg);
-void CB_setObstacleVelocity(const geometry_msgs::TwistConstPtr& twist_msg, const unsigned int id);
-
+void CB_mainCycle(const ros::TimerEvent &e);
+void CB_publishCycle(const ros::TimerEvent &e);
+void CB_reconfigure(TebLocalPlannerReconfigureConfig &reconfig, uint32_t level);
+void CB_customObstacle(
+    const costmap_converter::ObstacleArrayMsg::ConstPtr &obst_msg);
+void CreateInteractiveMarker(
+    const double &init_x, const double &init_y, unsigned int id,
+    std::string frame,
+    interactive_markers::InteractiveMarkerServer *marker_server,
+    interactive_markers::InteractiveMarkerServer::FeedbackCallback feedback_cb);
+void CB_obstacle_marker(
+    const visualization_msgs::InteractiveMarkerFeedbackConstPtr &feedback);
+void CB_clicked_points(const geometry_msgs::PointStampedConstPtr &point_msg);
+void CB_via_points(const nav_msgs::Path::ConstPtr &via_points_msg);
+void CB_setObstacleVelocity(const geometry_msgs::TwistConstPtr &twist_msg,
+                            const unsigned int id);
 
 // =============== Main function =================
-int main( int argc, char** argv )
-{
+int main(int argc, char **argv) {
   ros::init(argc, argv, "test_optim_node");
   ros::NodeHandle n("~");
- 
-  
+
   // load ros parameters from node handle
   config.loadRosParamFromNodeHandle(n);
- 
+
   ros::Timer cycle_timer = n.createTimer(ros::Duration(0.025), CB_mainCycle);
   ros::Timer publish_timer = n.createTimer(ros::Duration(0.1), CB_publishCycle);
-  
+
   // setup dynamic reconfigure
-  dynamic_recfg = boost::make_shared< dynamic_reconfigure::Server<TebLocalPlannerReconfigureConfig> >(n);
-  dynamic_reconfigure::Server<TebLocalPlannerReconfigureConfig>::CallbackType cb = boost::bind(CB_reconfigure, _1, _2);
+  dynamic_recfg = boost::make_shared<
+      dynamic_reconfigure::Server<TebLocalPlannerReconfigureConfig>>(n);
+  dynamic_reconfigure::Server<TebLocalPlannerReconfigureConfig>::CallbackType
+      cb = boost::bind(CB_reconfigure, _1, _2);
   dynamic_recfg->setCallback(cb);
-  
+
   // setup callback for custom obstacles
   custom_obst_sub = n.subscribe("obstacles", 1, CB_customObstacle);
-  
-  // setup callback for clicked points (in rviz) that are considered as via-points
+
+  // setup callback for clicked points (in rviz) that are considered as
+  // via-points
   clicked_points_sub = n.subscribe("/clicked_point", 5, CB_clicked_points);
-  
-  // setup callback for via-points (callback overwrites previously set via-points)
+
+  // setup callback for via-points (callback overwrites previously set
+  // via-points)
   via_points_sub = n.subscribe("via_points", 1, CB_via_points);
 
   // interactive marker server for simulated dynamic obstacles
-  interactive_markers::InteractiveMarkerServer marker_server("marker_obstacles");
+  interactive_markers::InteractiveMarkerServer marker_server(
+      "marker_obstacles");
 
-  obst_vector.push_back( boost::make_shared<PointObstacle>(-3,1) );
-  obst_vector.push_back( boost::make_shared<PointObstacle>(6,2) );
-  obst_vector.push_back( boost::make_shared<PointObstacle>(0,0.1) );
-//  obst_vector.push_back( boost::make_shared<LineObstacle>(1,1.5,1,-1.5) ); //90 deg
-//  obst_vector.push_back( boost::make_shared<LineObstacle>(1,0,-1,0) ); //180 deg
-//  obst_vector.push_back( boost::make_shared<PointObstacle>(-1.5,-0.5) );
+  obst_vector.push_back(boost::make_shared<PointObstacle>(-3, 1));
+  obst_vector.push_back(boost::make_shared<PointObstacle>(6, 2));
+  obst_vector.push_back(boost::make_shared<PointObstacle>(0, 0.1));
+  //  obst_vector.push_back( boost::make_shared<LineObstacle>(1,1.5,1,-1.5) );
+  //  //90 deg obst_vector.push_back( boost::make_shared<LineObstacle>(1,0,-1,0)
+  //  ); //180 deg obst_vector.push_back(
+  //  boost::make_shared<PointObstacle>(-1.5,-0.5) );
 
   // Dynamic obstacles
-  Eigen::Vector2d vel (0.1, -0.3);
+  Eigen::Vector2d vel(0.1, -0.3);
   obst_vector.at(0)->setCentroidVelocity(vel);
   vel = Eigen::Vector2d(-0.3, -0.2);
   obst_vector.at(1)->setCentroidVelocity(vel);
@@ -122,39 +133,44 @@ int main( int argc, char** argv )
   polyobst->pushBackVertex(0, 1);
   polyobst->pushBackVertex(1, 1);
   polyobst->pushBackVertex(2, 1);
- 
+
   polyobst->finalizePolygon();
   obst_vector.emplace_back(polyobst);
   */
-  
-  for (unsigned int i=0; i<obst_vector.size(); ++i)
-  {
-    // setup callbacks for setting obstacle velocities
-    std::string topic = "/test_optim_node/obstacle_" + std::to_string(i) + "/cmd_vel";
-    obst_vel_subs.push_back(n.subscribe<geometry_msgs::Twist>(topic, 1, boost::bind(&CB_setObstacleVelocity, _1, i)));
 
-    //CreateInteractiveMarker(obst_vector.at(i)[0],obst_vector.at(i)[1],i,&marker_server, &CB_obstacle_marker);  
+  for (unsigned int i = 0; i < obst_vector.size(); ++i) {
+    // setup callbacks for setting obstacle velocities
+    std::string topic =
+        "/test_optim_node/obstacle_" + std::to_string(i) + "/cmd_vel";
+    obst_vel_subs.push_back(n.subscribe<geometry_msgs::Twist>(
+        topic, 1, boost::bind(&CB_setObstacleVelocity, _1, i)));
+
+    // CreateInteractiveMarker(obst_vector.at(i)[0],obst_vector.at(i)[1],i,&marker_server,
+    // &CB_obstacle_marker);
     // Add interactive markers for all point obstacles
-    boost::shared_ptr<PointObstacle> pobst = boost::dynamic_pointer_cast<PointObstacle>(obst_vector.at(i));
-    if (pobst)
-    {
-      CreateInteractiveMarker(pobst->x(),pobst->y(),i, config.map_frame, &marker_server, &CB_obstacle_marker);  
+    boost::shared_ptr<PointObstacle> pobst =
+        boost::dynamic_pointer_cast<PointObstacle>(obst_vector.at(i));
+    if (pobst) {
+      CreateInteractiveMarker(pobst->x(), pobst->y(), i, config.map_frame,
+                              &marker_server, &CB_obstacle_marker);
     }
   }
   marker_server.applyChanges();
-  
+
   // Setup visualization
   visual = TebVisualizationPtr(new TebVisualization(n, config));
-  
+
   // Setup robot shape model
-  RobotFootprintModelPtr robot_model = TebLocalPlannerROS::getRobotFootprintFromParamServer(n, config);
-  
+  RobotFootprintModelPtr robot_model =
+      TebLocalPlannerROS::getRobotFootprintFromParamServer(n, config);
+
   // Setup planner (homotopy class planning or just the local teb planner)
   if (config.hcp.enable_homotopy_class_planning)
-    planner = PlannerInterfacePtr(new HomotopyClassPlanner(config, &obst_vector, robot_model, visual, &via_points));
+    planner = PlannerInterfacePtr(new HomotopyClassPlanner(
+        config, &obst_vector, robot_model, visual, &via_points));
   else
-    planner = PlannerInterfacePtr(new TebOptimalPlanner(config, &obst_vector, robot_model, visual, &via_points));
-  
+    planner = PlannerInterfacePtr(new TebOptimalPlanner(
+        config, &obst_vector, robot_model, visual, &via_points));
 
   no_fixed_obstacles = obst_vector.size();
   ros::spin();
@@ -163,32 +179,36 @@ int main( int argc, char** argv )
 }
 
 // Planning loop
-void CB_mainCycle(const ros::TimerEvent& e)
-{
-  planner->plan(PoseSE2(-4,0,0), PoseSE2(4,0,0)); // hardcoded start and goal for testing purposes
+void CB_mainCycle(const ros::TimerEvent &e) {
+  planner->plan(
+      PoseSE2(-4, 0, 0),
+      PoseSE2(4, 0, 0)); // hardcoded start and goal for testing purposes
 }
 
 // Visualization loop
-void CB_publishCycle(const ros::TimerEvent& e)
-{
+void CB_publishCycle(const ros::TimerEvent &e) {
   planner->visualize();
   visual->publishObstacles(obst_vector);
   visual->publishViaPoints(via_points);
 }
 
-void CB_reconfigure(TebLocalPlannerReconfigureConfig& reconfig, uint32_t level)
-{
+void CB_reconfigure(TebLocalPlannerReconfigureConfig &reconfig,
+                    uint32_t level) {
   config.reconfigure(reconfig);
 }
 
-void CreateInteractiveMarker(const double& init_x, const double& init_y, unsigned int id, std::string frame, interactive_markers::InteractiveMarkerServer* marker_server, interactive_markers::InteractiveMarkerServer::FeedbackCallback feedback_cb)
-{
+void CreateInteractiveMarker(
+    const double &init_x, const double &init_y, unsigned int id,
+    std::string frame,
+    interactive_markers::InteractiveMarkerServer *marker_server,
+    interactive_markers::InteractiveMarkerServer::FeedbackCallback
+        feedback_cb) {
   // create an interactive marker for our server
   visualization_msgs::InteractiveMarker i_marker;
   i_marker.header.frame_id = frame;
   i_marker.header.stamp = ros::Time::now();
   std::ostringstream oss;
-  //oss << "obstacle" << id;
+  // oss << "obstacle" << id;
   oss << id;
   i_marker.name = oss.str();
   i_marker.description = "Obstacle";
@@ -212,10 +232,10 @@ void CreateInteractiveMarker(const double& init_x, const double& init_y, unsigne
   // create a non-interactive control which contains the box
   visualization_msgs::InteractiveMarkerControl box_control;
   box_control.always_visible = true;
-  box_control.markers.push_back( box_marker );
+  box_control.markers.push_back(box_marker);
 
   // add the control to the interactive marker
-  i_marker.controls.push_back( box_control );
+  i_marker.controls.push_back(box_control);
 
   // create a control which will move the box, rviz will insert 2 arrows
   visualization_msgs::InteractiveMarkerControl move_control;
@@ -224,102 +244,98 @@ void CreateInteractiveMarker(const double& init_x, const double& init_y, unsigne
   move_control.orientation.x = 0;
   move_control.orientation.y = 0.707107f;
   move_control.orientation.z = 0;
-  move_control.interaction_mode = visualization_msgs::InteractiveMarkerControl::MOVE_PLANE;
-
+  move_control.interaction_mode =
+      visualization_msgs::InteractiveMarkerControl::MOVE_PLANE;
 
   // add the control to the interactive marker
   i_marker.controls.push_back(move_control);
 
   // add the interactive marker to our collection
   marker_server->insert(i_marker);
-  marker_server->setCallback(i_marker.name,feedback_cb);
+  marker_server->setCallback(i_marker.name, feedback_cb);
 }
 
-void CB_obstacle_marker(const visualization_msgs::InteractiveMarkerFeedbackConstPtr &feedback)
-{
+void CB_obstacle_marker(
+    const visualization_msgs::InteractiveMarkerFeedbackConstPtr &feedback) {
   std::stringstream ss(feedback->marker_name);
   unsigned int index;
   ss >> index;
-  
-  if (index>=no_fixed_obstacles) 
+
+  if (index >= no_fixed_obstacles)
     return;
-  PointObstacle* pobst = static_cast<PointObstacle*>(obst_vector.at(index).get());
-  pobst->position() = Eigen::Vector2d(feedback->pose.position.x,feedback->pose.position.y);	  
+  PointObstacle *pobst =
+      static_cast<PointObstacle *>(obst_vector.at(index).get());
+  pobst->position() =
+      Eigen::Vector2d(feedback->pose.position.x, feedback->pose.position.y);
 }
 
-void CB_customObstacle(const costmap_converter::ObstacleArrayMsg::ConstPtr& obst_msg)
-{
-  // resize such that the vector contains only the fixed obstacles specified inside the main function
+void CB_customObstacle(
+    const costmap_converter::ObstacleArrayMsg::ConstPtr &obst_msg) {
+  // resize such that the vector contains only the fixed obstacles specified
+  // inside the main function
   obst_vector.resize(no_fixed_obstacles);
-  
-  // Add custom obstacles obtained via message (assume that all obstacles coordiantes are specified in the default planning frame)  
-  for (size_t i = 0; i < obst_msg->obstacles.size(); ++i)
-  {
-    if (obst_msg->obstacles.at(i).polygon.points.size() == 1 )
-    {
-      if (obst_msg->obstacles.at(i).radius == 0) 
-      {
-        obst_vector.push_back(ObstaclePtr(new PointObstacle( obst_msg->obstacles.at(i).polygon.points.front().x,
-                                                           obst_msg->obstacles.at(i).polygon.points.front().y )));
+
+  // Add custom obstacles obtained via message (assume that all obstacles
+  // coordiantes are specified in the default planning frame)
+  for (size_t i = 0; i < obst_msg->obstacles.size(); ++i) {
+    if (obst_msg->obstacles.at(i).polygon.points.size() == 1) {
+      if (obst_msg->obstacles.at(i).radius == 0) {
+        obst_vector.push_back(ObstaclePtr(new PointObstacle(
+            obst_msg->obstacles.at(i).polygon.points.front().x,
+            obst_msg->obstacles.at(i).polygon.points.front().y)));
+      } else {
+        obst_vector.push_back(ObstaclePtr(new CircularObstacle(
+            obst_msg->obstacles.at(i).polygon.points.front().x,
+            obst_msg->obstacles.at(i).polygon.points.front().y,
+            obst_msg->obstacles.at(i).radius)));
       }
-      else
-      {
-        obst_vector.push_back(ObstaclePtr(new CircularObstacle( obst_msg->obstacles.at(i).polygon.points.front().x,
-                                                            obst_msg->obstacles.at(i).polygon.points.front().y,
-                                                            obst_msg->obstacles.at(i).radius )));
-      }
-    }
-    else if (obst_msg->obstacles.at(i).polygon.points.empty())
-    {
-      ROS_WARN("Invalid custom obstacle received. List of polygon vertices is empty. Skipping...");
+    } else if (obst_msg->obstacles.at(i).polygon.points.empty()) {
+      ROS_WARN("Invalid custom obstacle received. List of polygon vertices is "
+               "empty. Skipping...");
       continue;
-    }
-    else
-    {
-      PolygonObstacle* polyobst = new PolygonObstacle;
-      for (size_t j=0; j<obst_msg->obstacles.at(i).polygon.points.size(); ++j)
-      {
-        polyobst->pushBackVertex( obst_msg->obstacles.at(i).polygon.points[j].x,
-                                  obst_msg->obstacles.at(i).polygon.points[j].y );
+    } else {
+      PolygonObstacle *polyobst = new PolygonObstacle;
+      for (size_t j = 0; j < obst_msg->obstacles.at(i).polygon.points.size();
+           ++j) {
+        polyobst->pushBackVertex(obst_msg->obstacles.at(i).polygon.points[j].x,
+                                 obst_msg->obstacles.at(i).polygon.points[j].y);
       }
       polyobst->finalizePolygon();
       obst_vector.push_back(ObstaclePtr(polyobst));
     }
 
-    if(!obst_vector.empty())
-      obst_vector.back()->setCentroidVelocity(obst_msg->obstacles.at(i).velocities, obst_msg->obstacles.at(i).orientation);
+    if (!obst_vector.empty())
+      obst_vector.back()->setCentroidVelocity(
+          obst_msg->obstacles.at(i).velocities,
+          obst_msg->obstacles.at(i).orientation);
   }
 }
 
-
-void CB_clicked_points(const geometry_msgs::PointStampedConstPtr& point_msg)
-{
-  // we assume for simplicity that the fixed frame is already the map/planning frame
-  // consider clicked points as via-points
-  via_points.push_back( Eigen::Vector2d(point_msg->point.x, point_msg->point.y) );
-  ROS_INFO_STREAM("Via-point (" << point_msg->point.x << "," << point_msg->point.y << ") added.");
-  if (config.optim.weight_viapoint<=0)
+void CB_clicked_points(const geometry_msgs::PointStampedConstPtr &point_msg) {
+  // we assume for simplicity that the fixed frame is already the map/planning
+  // frame consider clicked points as via-points
+  via_points.push_back(Eigen::Vector2d(point_msg->point.x, point_msg->point.y));
+  ROS_INFO_STREAM("Via-point (" << point_msg->point.x << ","
+                                << point_msg->point.y << ") added.");
+  if (config.optim.weight_viapoint <= 0)
     ROS_WARN("Note, via-points are deactivated, since 'weight_via_point' <= 0");
 }
 
-void CB_via_points(const nav_msgs::Path::ConstPtr& via_points_msg)
-{
+void CB_via_points(const nav_msgs::Path::ConstPtr &via_points_msg) {
   ROS_INFO_ONCE("Via-points received. This message is printed once.");
   via_points.clear();
-  for (const geometry_msgs::PoseStamped& pose : via_points_msg->poses)
-  {
+  for (const geometry_msgs::PoseStamped &pose : via_points_msg->poses) {
     via_points.emplace_back(pose.pose.position.x, pose.pose.position.y);
   }
 }
 
-void CB_setObstacleVelocity(const geometry_msgs::TwistConstPtr& twist_msg, const unsigned int id)
-{
-  if (id >= obst_vector.size())
-  {
+void CB_setObstacleVelocity(const geometry_msgs::TwistConstPtr &twist_msg,
+                            const unsigned int id) {
+  if (id >= obst_vector.size()) {
     ROS_WARN("Cannot set velocity: unknown obstacle id.");
     return;
   }
 
-  Eigen::Vector2d vel (twist_msg->linear.x, twist_msg->linear.y);
+  Eigen::Vector2d vel(twist_msg->linear.x, twist_msg->linear.y);
   obst_vector.at(id)->setCentroidVelocity(vel);
 }
